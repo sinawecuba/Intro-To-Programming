570a02b30d7b8c895c6b9cffc6889bdaca626cbf
sinawecuba <sinawesome@gmail.com>
2025-07-08 14:49:12 +0200

    Add files via upload

---
+++ Book 1 JavaScrpit for Kids/Source Code/chapter 2/Try-Out (P17)/Balloons.js
@@ -0,0 +1,6 @@
+//How many balloons will we need
+// Add 15 and 9, then multiply the result by 2
+var result = (15 + 9) * 2; // (15 + 9) = 24 → 24 * 2 = 48
+
+// Output the result to the console
+console.log(result); // Logs: 48
