668591e7d213c010bfda4ef66d4968a2db9180ed
sinawecuba <sinawesome@gmail.com>
2025-07-08 18:11:25 +0200

    Add files via upload

---
+++ Book 1 JavaScrpit for Kids/Source Code/chapter 2/Try-Out (P23)/balloons.js
@@ -0,0 +1,16 @@
+// Declare a variable named 'balloons' and set its initial value to 100
+var balloons = 100;
+
+// Multiply the value of 'balloons' by 2 using the *= operator (balloons = balloons * 2)
+balloons *= 2;
+
+// The value of 'balloons' is now 200
+
+
+// Declare a new variable named 'balloons' and set it again to 100
+var balloons = 100;
+
+// Divide the value of 'balloons' by 4 using the /= operator (balloons = balloons / 4)
+balloons /= 4;
+
+// The value of 'balloons' is now 25
