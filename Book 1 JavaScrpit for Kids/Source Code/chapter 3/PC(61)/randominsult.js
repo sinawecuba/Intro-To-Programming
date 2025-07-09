c8a11ad7e12363701981967a94969e8ea2348e56
sinawecuba <sinawesome@gmail.com>
2025-07-09 20:11:02 +0200

    Add files via upload

---
+++ Book 1 JavaScrpit for Kids/Source Code/chapter 3/PC(61)/randominsult.js
@@ -0,0 +1,24 @@
+var randomFamilyMember = ["Mom", "Dad", "Brother", "Grandma"];
+var randomAdjectives = ["older", "fatter", "dumber", "Uglier"];
+var randomAnimal = ["Rat's", "Snake's", "Donakey's", "Cockroach's", "Frog's"];
+var randomBodyPart = ["Feet", "Armpits", "Toenail", "Belly Button", "Butt"]
+
+// Pick a random Family member from the randomFamilyMember array:
+var randomFamilyMember = randomFamilyMember[Math.floor(Math.random() * 4)];
+
+// Pick a random adjective from the randomAdjectives array:
+var randomAdjective = randomAdjectives[Math.floor(Math.random() * 4)];
+
+// Pick a random animal from the randomAnimal array:
+var randomAnimal = randomAnimal[Math.floor(Math.random() * 
+ randomAnimal.length)];
+
+ //Pick a random animal body part from randomBodyPart
+var randomBodyPart = randomBodyPart[Math.floor(Math.random() * 
+ randomBodyPart.length)]
+
+// Join all the random strings into a sentence:
+var randomInsult = "Your " + randomFamilyMember + " is " + randomAdjective + " than a " +  randomAnimal + " " + randomBodyPart + "!!!";
+
+console.log(randomInsult);
+
