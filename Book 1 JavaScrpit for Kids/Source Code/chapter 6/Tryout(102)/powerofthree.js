7b0d123fc6748f1c1dade3007c9e1329e6711d72
sinawecuba <sinawesome@gmail.com>
2025-07-12 14:37:25 +0200

    Add files via upload

---
+++ Book 1 JavaScrpit for Kids/Source Code/chapter 6/Tryout(102)/powerofthree.js
@@ -0,0 +1,5 @@
+console.log("----the powers of 3 under 10,000----")
+
+for (var x = 3; x < 10000; x = x * 3) {   // Start at 3, keep looping while x is less than 10,000, and multiply x by 3 each time
+  console.log(x);                         // Print the current value of x
+}
