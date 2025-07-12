9e5b8aba0d639e8b1614bf52c7151a3f52a5360b
sinawecuba <sinawesome@gmail.com>
2025-07-12 14:22:36 +0200

    Add files via upload

---
+++ Book 1 JavaScrpit for Kids/Source Code/chapter 6/Tryout(96)/hellostranger.js
@@ -0,0 +1,18 @@
+let name = "Sinawe";                          // Declare a variable 'name' and assign it the value "Sinawe"
+
+if (name === "Sinawe") {                      // Check if the value of 'name' is exactly equal to "Sinawe"
+    console.log("Hello Me!");                 // If true, print "Hello Me!" to the console
+} else {
+    console.log("Hello Stranger!");           // If false, print "Hello Stranger!" instead
+}
+
+let name2 = "Sinawe";                         // Declare a second variable 'name2' and assign it "Sinawe"
+
+if (name2 === "Jenny") {                      // Check if 'name2' is exactly equal to "Jenny"
+    console.log("Hello Me!");                 // If true, print "Hello Me!"
+} else {
+    console.log("Hello Stranger!");           // If false (which it is), print "Hello Stranger!"
+}
+
+
+    console.log("-------Mom & Dad-------")
