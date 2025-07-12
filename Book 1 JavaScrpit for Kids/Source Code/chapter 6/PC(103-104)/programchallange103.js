0ca51e3b1d16ca3b46ba70e393d967986a36e2f7
sinawecuba <sinawesome@gmail.com>
2025-07-12 14:57:45 +0200

    Add files via upload

---
+++ Book 1 JavaScrpit for Kids/Source Code/chapter 6/PC(103-104)/programchallange103.js
@@ -0,0 +1,8 @@
+console.log("1.")
+console.log("-----Awesome Animals-----")
+
+ var animals = ["Cat", "Fish", "Lemur", "Komodo Dragon"];
+for (var w = 0; w < animals.length; w++) {                                           
+  animals[w] = "Awesome " + animals[w];            
+}
+console.log(animals);
