930629dda3f9175aca30ec81db8dcc2f033dccd2
sinawecuba <sinawesome@gmail.com>
2025-07-12 15:00:40 +0200

    Add files via upload

--- Book 1 JavaScrpit for Kids/Source Code/chapter 6/PC(103-104)/programchallange103.js
+++ Book 1 JavaScrpit for Kids/Source Code/chapter 6/PC(103-104)/programchallange103.js
@@ -1,8 +1,14 @@
-console.log("1.")
-console.log("-----Awesome Animals-----")
+console.log("-----1. Awesome Animals-----")
 
- var animals = ["Cat", "Fish", "Lemur", "Komodo Dragon"];
-for (var w = 0; w < animals.length; w++) {                                           
-  animals[w] = "Awesome " + animals[w];            
+var animals = ["Cat", "Fish", "Lemur", "Komodo Dragon"]; // Create an array of animal names
+
+for (var w = 0; w < animals.length; w++) {               // Loop through each index of the animals array
+  animals[w] = "Awesome " + animals[w];                  // Add "Awesome " in front of each animal name
 }
-console.log(animals);
+console.log(animals);                                    // Print the updated array to the console
+
+
+
+console.log("-----2.Random String Generator-----")
+
+
