8ce6525cc263978edf7a98453a9d46019ba41d4b
sinawecuba <sinawesome@gmail.com>
2025-07-13 10:49:45 +0200

    Add files via upload

--- Book 1 JavaScrpit for Kids/Source Code/chapter 6/PC(103-104)/programchallange103.js
+++ Book 1 JavaScrpit for Kids/Source Code/chapter 6/PC(103-104)/programchallange103.js
@@ -21,3 +21,25 @@ while (randomString.length < 16) {                  // Keep looping until the st
 
 console.log(randomString);                          // Print the final random string
 
+console.log("-----3. h4ck3r sp34k-----")
+
+var input = "javascript is awesome";                // Original string to transform
+var output = "";                                     // Empty string to build the transformed result
+
+for (var i = 0; i < input.length; i++) {             // Loop through each character in the input string
+  var letter = input[i];                             // Get the current letter
+
+  if (letter === "a") {                              // If the letter is "a"
+    output += "4";                                   // Replace it with "4"
+  } else if (letter === "e") {                       // If the letter is "e"
+    output += "3";                                   // Replace it with "3"
+  } else if (letter === "i") {                       // If the letter is "i"
+    output += "1";                                   // Replace it with "1"
+  } else if (letter === "o") {                       // If the letter is "o"
+    output += "0";                                   // Replace it with "0"
+  } else {                                           // For any other letter
+    output += letter;                                // Just add it as it is
+  }
+}
+
+console.log(output);                                 // Print the final "leetified" output
