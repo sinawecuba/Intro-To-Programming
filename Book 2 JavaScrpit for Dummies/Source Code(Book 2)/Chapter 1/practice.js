573eb1b557c9b9f7595dbe5083bef2f71a01e837
sinawecuba <sinawesome@gmail.com>
2025-07-14 11:28:46 +0200

    Add files via upload

---
+++ Book 2 JavaScrpit for Dummies/Source Code(Book 2)/Chapter 1/practice.js
@@ -0,0 +1,4 @@
+console.log(2000 - 37);                  // Subtracts 37 from 2000 → prints 1963
+console.log(30 * 27);                    // Multiplies 30 by 27 → prints 810
+console.log(120 / 20);                   // Divides 120 by 20 → prints 6
+console.log("Your name" + " " + "is learning JavaScript!");  // Combines strings → prints "Your name is learning JavaScript!"
