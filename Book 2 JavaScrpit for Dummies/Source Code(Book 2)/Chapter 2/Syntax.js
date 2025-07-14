19bbb8b9404ea12ee8a36bb1de548ece441cd63f
sinawecuba <sinawesome@gmail.com>
2025-07-14 12:13:59 +0200

    Add files via upload

---
+++ Book 2 JavaScrpit for Dummies/Source Code(Book 2)/Chapter 2/Syntax.js
@@ -0,0 +1,23 @@
+alert("Eating Fruit is Delicious!");                                  // Shows a popup alert with the message
+
+for (var i = 0; i < 10; i++) {                             // Loop 10 times
+  document.write("Coding is fun!<br>");                    // Write message to the page with a line break
+}
+
+for (var i = 0; i < 5; i++) {                              // Loop 5 times
+  document.write("Joe said, \"Hi!\"<br>");                 // Write quoted text with escaped quotes
+}
+
+for (var i = 0; i < 8; i++) {                              // Loop 5 times
+  document.write("Liverpool FC, \"won the league!\"<br>");                 // Write quoted text with escaped quotes
+}
+
+/* 
+-----------multi comment line----------
+
+Write some text directly to the HTML output by using document.write().
+document.write() still works perfectly in all major browsers for now, 
+but it's deprecated, meaning it's discouraged and may be removed in future versions.
+
+\" is an escape character. It tells JavaScript to treat the double quote as a character, not the end of the string.
+*/
